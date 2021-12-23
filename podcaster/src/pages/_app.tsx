e6e9fe60932234d35f2d6c402e84d388086223c7
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-23 16:53:17 -0300

    Segunda Aula
    Completa

---
+++ podcaster/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import "../styles/global.scss";
+import styles from "../styles/app.module.scss";
+import { Player } from "../components/Player";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />;
+      </main>
+
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ podcaster/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { GetStaticProps } from "next";
+
+export default function Home(props) {
+  return (
+    <>
+      <h1>oi</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
