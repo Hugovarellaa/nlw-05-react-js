64ef67464ed5742870b1b0fa4d79f49cbdd8df01
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-24 00:30:57 -0300

    Terceira Aula incompleta
    38:54

--- podcaster/src/pages/index.tsx
+++ podcaster/src/pages/index.tsx
@@ -1,20 +1,91 @@
 import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationtoTimeString } from "../util/convertDurationtoTimeString";
+import styles from "./home.module.scss";
 
-export default function Home(props) {
+interface Episode {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  publishedAt: string;
+  members: string;
+  durationsAsString: string;
+  url: string;
+  duration: string;
+}
+interface HomeProps {
+  allEpisodes: Episode[];
+  latesEpisodes: Episode[];
+}
+
+export default function Home({ allEpisodes, latesEpisodes }: HomeProps) {
   return (
-    <>
-      <h1>oi</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </>
+    <div className={styles.homepage}>
+      <section className={styles.latesEpisodes}>
+        <h2>Ultimos lançamentos</h2>
+        <ul>
+          {latesEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                <img src={episode.thumbnail} alt={episode.title} />
+
+                <div className={styles.episodeDetails}>
+                  <a href={episode.url}>
+                    <p>{episode.members}</p>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationsAsString}</span>
+                  </a>
+                </div>
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódeo" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      member: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationsAsString: convertDurationtoTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latesEpisodes = episodes.splice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return {
     props: {
-      episodes: data,
+      allEpisodes,
+      latesEpisodes,
     },
     revalidate: 60 * 60 * 24,
   };
